8c4cef46c3e7145186f10eb58509b0c65f4d4949
Daemetry <hrudmirom@gmail.com>
2024-04-16 17:49:57 +0500

    Modal added

--- index.js
+++ index.js
@@ -18,3 +18,35 @@ document.querySelector(".add-button").addEventListener("click", () => {
     });
     forms[forms.length - 1].after(newForm);
 });
+
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-content");
+const overlay = document.querySelector(".modal-overlay");
+
+function declineDrink(n) {
+    if (n % 10 === 1 && n % 100 !== 11) {
+        return "напиток";
+    } else if (n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20)) {
+        return "напитка";
+    } else {
+        return "напитков";
+    }
+}
+
+document.querySelector(".submit-button")
+    .addEventListener('click',
+        (e) => {
+            e.preventDefault();
+            modalContent.textContent = `Заказ принят! Вы заказали ${count} ${declineDrink(count)}.`;
+            modal.style.display = 'block';
+            overlay.style.display = 'block';
+        }
+    );
+
+document.querySelector(".close-btn")
+    .addEventListener('click',
+        (e) => {
+            modal.style.display = 'none';
+            overlay.style.display = 'none';
+        }
+    );
